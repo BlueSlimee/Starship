43f395deed7bfc43dfb88bedfdd03d2fe80bec15
BlueSlimee beetch <blueslimexdfodeu@gmail.com>
2019-11-18 12:07:47 -0400

    fuck it

--- tests/requests.test.js
+++
@@ -1,6 +0,0 @@
-const RequestUtils = require('../src/utils/RequestUtils')
-const requestUtils = new RequestUtils({ _scopes: ['identify', 'guilds'] }) // We just want to check if there's any error so we don't need to pass a actual Starship instance
-
-test('tests if getUserData and its dependencies are working properly', () => {
-  expect(requestUtils.getUserData('a')).toBeNull()
-})
